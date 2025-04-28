1113a7c1794eee4bf603b8fc9e0c80026e909c4d
di0gosilva <diogo_silva8@estudante.sesisenai.org.br>
2025-04-28 14:56:23 -0300

    estrutura do projeto criada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between pt-3">
-      <Link to="/" className="flex items-center gap-1 lg:gap-2">
+    <header className="flex items-center justify-between mx-auto w-full px-10">
+      <Link to="/" className="flex items-center hover:text-[#00ADB5] transition duration-300">
         <h1 className="font-medium uppercase text-[18px]">LOGO</h1>
       </Link>
 
-      <nav className="flex gap-2">
-        <Link to="/">Home</Link>
-        <Link to="/movies">Filmes</Link>
+      <nav className="flex gap-5">
+        <Link className='hover:text-[#00ADB5] transition duration-300' to="/">Home</Link>
+        <Link className='hover:text-[#00ADB5] transition duration-300' to="/movies">Filmes</Link>
       </nav>
     </header>
   )
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -3,18 +3,20 @@ import Header from '../components/Header'
 
 export default function Layout() {
   return (
-    <div className='min-h-screen flex flex-col'>
-      <div className='mx-auto max-w-[1024px] w-full px-3'>
+    <div className='min-h-screen flex flex-col bg-[#121212] text-white'>
+      <div className='bg-[#1E1E1E] py-3'>
         <Header />
       </div>
 
-      <div className='flex flex-1 mx-auto max-w-[1024px] w-full px-3'>
+      <div className='flex flex-1 mx-auto w-full px-10'>
         <Outlet />
       </div>
 
-      <footer className="h-10 flex items-center justify-between max-w-[1024px] w-full px-3 mx-auto text-sm">
-        <p>Todos os direitos reservados.</p>
-        <p>&copy; Diogo Silva 2025</p>
+      <footer className="text-sm bg-[#1E1E1E] py-3">
+        <div className=' w-full px-10 mx-auto flex items-center justify-between '>
+          <p>Todos os direitos reservados.</p>
+          <p>&copy; Diogo Silva 2025</p>
+        </div>
       </footer>
     </div>
   )
